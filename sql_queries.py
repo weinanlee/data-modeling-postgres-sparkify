100bb27df8e2368465c557592a60ff06a5f4f278
Weinan Li <weianlee0322@gmail.com>
2020-04-26 13:22:47 +0200

    Frist try of creating and deleteing tables.

--- sql_queries.py
+++ sql_queries.py
@@ -1,28 +1,54 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE SONGPLAYS"
+user_table_drop = "DROP TABLE USERS"
+song_table_drop = "DROP SONGS"
+artist_table_drop = "DROP TABLE ARTISTS"
+time_table_drop = "DROP TABLE TIME"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS SONGPLAYS ()
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS SONGPLAYS (songplay_id int,
+	start_time varchar,
+    user_id int,
+    level varchar, 
+    song_id varchar, 
+    artist_id int, 
+    session_id int, 
+    location varchar, 
+    user_agent varchar
+)
 
 """)
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE IF NOT EXISTS USERS (user_id int, 
+	first_name varchar, 
+	last_name varchar, 
+	gender varchar, 
+	level varchar)
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXISTS SONGS (song_id int, 
+	title varchar, 
+	artist_id varchar, 
+	year int, 
+	duration numeric)
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS ARTISTS (artist_id varchar, 
+	name varchar, 
+	location varchar, 
+	latitude varchar, 
+	longitude varchar)
 """)
 
-time_table_create = ("""
+time_table_create = ("""CREATE TABLE IF NOT EXISTS TIME (start_time varchar, 
+	hour varchar, 
+	day varchar, 
+	week varchar, 
+	month varchar, 
+	year varchar, 
+	weekday varchar)
 """)
 
 # INSERT RECORDS
